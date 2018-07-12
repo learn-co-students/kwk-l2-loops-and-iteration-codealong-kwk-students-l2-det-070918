1803e29e47f8d81bdff8968a4adf5da6b5e916c8
SoundBrake <mesuper222@gmail.com>
2018-07-12 15:35:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+
+function printBadges(array){
+  for(var i = 0; i< array.length ; i++){
+    console.log("Welcome " + array[i] + "! You are employee #" + (i+1)+ ".")
+  }
+  return array
+}
+
+
+function tailsNeverFails(){
+  heads = false;
+  tails = 0;
+  while(heads === false){
+    var num = Math.random()
+    if(num >= 0.5){
+      tails++
+    }else{
+      heads = true
+    }
+  }
+  
+  return "You got " + tails + " tails in a row!"
+}

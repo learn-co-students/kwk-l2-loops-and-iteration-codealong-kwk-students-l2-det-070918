3cfa66fcf3fee632d045014660fe17b9995042d2
cdevries546 <cdevries@comcast.net>
2018-07-12 19:52:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solutions in this file
+// var gifts =['drone', 'Jordans', 'clothes', 'corgi', 'books','pomsky']
+// function wrapGift(gifts) {
+//   console.log(gifts+ "has been wrapped")
+// }
+// for (var counter = 0; counter < gifts.length; counter++){
+//   var gift =gifts[counter]
+//   wrapGift(gift)
+
+function printBadges(array){
+  for(var i = 0; i< array.length ; i++){
+    console.log("Welcome " + array[i] + "! You are employee #" + (i+1)+ ".")
+  }
+  return array
+}
+
+
+function tailsNeverFails(){
+  heads = false;
+  tails = 0;
+  while(heads === false){
+    var num = Math.random()
+    if(num >= 0.5){
+      tails++
+    }else{
+      heads = true
+    }
+  }
+  
+  return "You got " + tails + " tails in a row!"
+}
